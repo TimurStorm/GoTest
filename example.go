3cb00b20a120936635c79c231e828ff6fcf64da4
Tim <noobofmylive@gmail.com>
2021-11-02 18:11:25 +0300

    новый файл-пример

--- example.go
+++ example.go
@@ -7,16 +7,28 @@ import (
 	"time"
 )
 
+func exampleTopFile() {
+	err := top3.GetTopFile("url.txt", "result.json", top3.WithTags([]string{"p", "a"}))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func exampleTopURL() {
+	result, err := top3.GetTop("habr.com")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+}
+
 func main() {
 	// Используем 1 ядро процессора
 	runtime.GOMAXPROCS(1)
 	// Для измерения времени
 	start := time.Now()
-	err := top3.GetTopFile("url.txt", "result.json", top3.WithTags([]string{"p", "a"}))
-	if err != nil {
-		fmt.Println(err)
-	}
-
+	exampleTopFile()
+	exampleTopURL()
 	// Вывод затраченного времени
 	fmt.Printf("Time spend: %v", time.Since(start))
 }
