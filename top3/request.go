6b12e68617d63797099c3c938e709dc7fd21cbe2
Tim <noobofmylive@gmail.com>
2021-11-15 18:12:34 +0300

    рефакторинг request.go: добавлены limitDo() и getTimeout()

--- top3/request.go
+++ top3/request.go
@@ -7,17 +7,58 @@ import (
 	"net/url"
 	"strconv"
 	"time"
-
-	"github.com/manucorporat/try"
 )
 
-// sendRequest отправляет запрос
-func sendRequest(u string, domain string, o ...Option) (*http.Response, error) {
+func limitDo(domain string, request *http.Request, options *AllOptions) (*http.Response, error) {
 
-	options := &AllOptions{}
-	for _, opt := range o {
-		opt(options)
+	// Для облегчения читаймости
+	hosts := options.hosts
+
+	// Проверяем наличие хоста в мапе повторяющихся
+	// Получаем количество подключений к данному хосту
+	hostCount, contain := hosts.Map[domain]
+
+	// Если не в мапе
+	if !contain {
+		resp, err := options.Client.Do(request)
+		if err != nil {
+			return resp, err
+		}
+
+		return resp, nil
+	} else {
+		// Ожидание пока не освободится место новому запросу
+		for hostCount >= options.HostReqLimit {
+			time.Sleep(100 * time.Millisecond)
+			hosts.MapMutex.Lock()
+			hostCount = hosts.Map[domain]
+			hosts.MapMutex.Unlock()
+		}
+
+		// + 1 запрос
+		hosts.MapMutex.RLock()
+		hosts.Map[domain] += 1
+		hosts.MapMutex.RUnlock()
+
+		// Отправка запроса
+		resp, err := options.Client.Do(request)
+
+		// - 1 запрос
+		hosts.MapMutex.RLock()
+		hosts.Map[domain] -= 1
+		hosts.MapMutex.RUnlock()
+
+		if err != nil {
+			return resp, err
+		}
+		return resp, nil
 	}
+}
+
+// sendRequest отправляет запрос
+func sendRequest(u string, domain string, options *AllOptions) (*http.Response, error) {
+
+	var resp *http.Response
 
 	// Создаём запрос
 	request, err := http.NewRequest("GET", u, nil)
@@ -25,65 +66,90 @@ func sendRequest(u string, domain string, o ...Option) (*http.Response, error) {
 		return nil, err
 	}
 
-	//Если нет ограничений на отправку запросов на 1 хост
-	//Просто отправляем запрос
-
+	// Если нет ограничений на отправку запросов на 1 хост
+	// Просто отправляем запрос
 	if options.HostReqLimit == 0 {
 
-		resp, err := options.Client.Do(request)
+		resp, err = options.Client.Do(request)
 		if err != nil {
 			return resp, err
 		}
-		return resp, nil
-
+		//
 	} else {
-		//Для облегчения читаймости
-		hosts := options.hosts
-
-		// Проверяем наличие хоста в мапе повторяющихся
-		// Получаем количество подключений к данному хосту
-		// hosts.MapMutex.Lock()
-		hostCount, contain := hosts.Map[domain]
-		// hosts.MapMutex.Unlock()
-
-		// Если не в мапе
-		if !contain {
-			resp, err := options.Client.Do(request)
-			if err != nil {
-				return resp, err
-			}
-
-			return resp, nil
-		} else {
-			fmt.Println(hosts.Map)
-			// Ожидание пока не освободится место новому запросу
-			for hostCount >= options.HostReqLimit {
-				time.Sleep(100 * time.Millisecond)
-				hosts.MapMutex.Lock()
-				hostCount = hosts.Map[domain]
-				hosts.MapMutex.Unlock()
-				fmt.Println("В очереди")
-			}
-
-			// + 1 запрос
-			hosts.MapMutex.RLock()
-			hosts.Map[domain] += 1
-			hosts.MapMutex.RUnlock()
-
-			// Отправка запроса
-			resp, err := options.Client.Do(request)
-
-			// - 1 запрос
-			hosts.MapMutex.RLock()
-			hosts.Map[domain] -= 1
-			hosts.MapMutex.RUnlock()
-
-			if err != nil {
-				return resp, err
-			}
-			return resp, nil
+		resp, err = limitDo(domain, request, options)
+		if err != nil {
+			return resp, err
 		}
 	}
+	return resp, nil
+}
+
+func getTimeout(resp *http.Response, defaultTimeout time.Duration) (time.Duration, error) {
+	// Получаем timeout
+	retryAfter := resp.Header.Values("Retry-After")
+
+	// Timeout
+	var timeout = defaultTimeout
+	var err error
+	if timeout == 0 {
+		timeout = 7 * time.Second
+	}
+
+	// Если был найден Retry-After в заголовках ответа
+	if len(retryAfter) > 0 {
+		count, err := strconv.Atoi(retryAfter[0])
+		if err == nil {
+			timeout = time.Duration(count) * time.Second
+			return timeout, nil
+		}
+
+		t, err := time.Parse(time.RFC1123, retryAfter[0])
+		if err == nil {
+			timeout = time.Since(t)
+			return timeout, nil
+		}
+	}
+	return timeout, err
+}
+
+// tryResend совершает попытку повторной отправки запроса
+func tryResend(resp *http.Response, u string, host string, options *AllOptions) (*http.Response, error) {
+	var (
+		newResp *http.Response
+		err     error
+		attempt uint
+	)
+
+	fmt.Printf("'%v' было получено. Попытка повторного отправки запроса на %v \n", resp.Status, u)
+
+	// Инициализируем timeout
+	// По умолчанию равен 7, может быть изменён из опцией AttemptTimeout
+	timeout, err := getTimeout(resp, options.AttemptTimeout)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("Таймаут - %v\n", timeout)
+
+	// Пытаемся получить хороший ответ от сервера
+	for resp.StatusCode != 200 {
+		if options.AttemptCount != 0 && options.AttemptCount < attempt {
+			break
+		}
+		time.Sleep(timeout)
+		newResp, err = sendRequest(u, host, options)
+		if err != nil {
+			return newResp, err
+		}
+
+		// Если была не найдена старница, невалиден запрос, запрещён доступ к ресурсу
+		if resp.StatusCode == 404 || resp.StatusCode == 400 || resp.StatusCode == 403 {
+			break
+		}
+		fmt.Printf("Попытка %v для %v статус %v \n", attempt, u, resp.Status)
+		attempt++
+	}
+	return newResp, nil
 }
 
 // getResponceBody обрабатывает запрос
@@ -103,7 +169,7 @@ func getResponceBody(u string, o ...Option) ([]byte, error) {
 	host := un.Hostname()
 
 	// Получаем ответ
-	resp, err := sendRequest(u, host, o...)
+	resp, err := sendRequest(u, host, options)
 	if err != nil {
 		return nil, err
 	}
@@ -112,59 +178,12 @@ func getResponceBody(u string, o ...Option) ([]byte, error) {
 
 	// В случае если было отправлено большое количество запросов в ближайшее время
 	if resp.StatusCode == 503 || resp.StatusCode == 429 {
-		fmt.Printf("'%v' было получено. Попытка повторного отправки запроса на %v \n", resp.Status, u)
 
-		// Получаем timeout
-		retryAfter := resp.Header.Values("Retry-After")
-
-		// Timeout
-		var timeout = options.AttemptTimeout
-		if timeout == 0 {
-			timeout = 7 * time.Second
+		resp, err = tryResend(resp, u, host, options)
+		if err != nil {
+			return nil, err
 		}
-
-		// Если был найден Retry-After в заголовках ответа
-		if len(retryAfter) > 0 {
-			try.This(func() {
-				count, err := strconv.Atoi(retryAfter[0])
-				if err != nil {
-					panic(err)
-				}
-				timeout = time.Duration(count) * time.Second
-			}).Catch(func(e try.E) {
-				t, err := time.Parse(time.RFC1123, retryAfter[0])
-				if err != nil {
-					panic(err)
-				}
-				timeout = time.Since(t)
-			})
-		}
-		fmt.Printf("Таймаут - %v\n", timeout)
-
-		// Счётчик попыток
-		var num uint = 0
-
-		// Пытаемся получить хороший ответ от сервера
-		for resp.StatusCode != 200 {
-			if options.AttemptCount != 0 && options.AttemptCount < num {
-				break
-			}
-			time.Sleep(timeout)
-			resp, err = sendRequest(u, host, o...)
-			if err != nil {
-				return nil, err
-			}
-
-			// Если была не найдена старница, невалиден запрос, запрещён доступ к ресурсу
-			if resp.StatusCode == 404 || resp.StatusCode == 400 || resp.StatusCode == 403 {
-				break
-			}
-			fmt.Printf("Попытка %v для %v статус %v \n", num, u, resp.Status)
-			num++
-		}
-
 		if resp.StatusCode != 200 {
-			// keepAlive := resp.Header.Values("keep-alive")
 			return nil, fmt.Errorf("http: %v ", resp.Status)
 		}
 	}
