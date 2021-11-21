1eb9e706f2bd0c1af3bce1353ddb02dc8b7e1118
Tim <noobofmylive@gmail.com>
2021-11-21 21:36:56 +0300

    добавлен downloadWorker
    добавлен воркер для отправки запросов, результаты которых могут обработаны по частям

--- top3/worker.go
+++ top3/worker.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"net/http"
 )
 
 // processWorker воркер для считывания урла и запуска top3.URL
@@ -55,5 +56,43 @@ func writeWorker(resultChan chan Result, errChan chan error, encoder *json.Encod
 		}
 		// Отправляем результат main горутине, что всё хорошо
 		errChan <- nil
+	}
+}
+
+func downloadWorker(byteChan chan []byte, urlChan chan string, errChan chan error, client *http.Client) {
+
+	for {
+		url, isOpen := <-urlChan
+		if !isOpen {
+			break
+		}
+
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			errChan <- err
+			return
+		}
+
+		res, err := client.Do(req)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		defer res.Body.Close()
+
+		reader := bufio.NewReader(res.Body)
+
+		for {
+			// Read the next chunk
+			bytes, err := reader.ReadBytes('\n')
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				errChan <- err
+				return
+			}
+			byteChan <- bytes
+		}
 	}
 }
