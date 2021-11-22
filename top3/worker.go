ff2b7990d8c46ade18c7040996a452602cb81e98
Tim <noobofmylive@gmail.com>
2021-11-22 19:27:05 +0300

    рефакторинг processWorker
    processWorker принимает данные от donloadWorker и считает количество упоминаний каждого слова. Результат отправляется по каналу в writeWorker, который записывает его в результирующий файл

--- top3/worker.go
+++ top3/worker.go
@@ -6,32 +6,91 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
+	"unicode/utf8"
+
+	"github.com/fatih/camelcase"
 )
 
 // processWorker воркер для считывания урла и запуска top3.URL
-func processWorker(resultChan chan Result, errChan chan error, reader *bufio.Reader, o ...Option) {
+func processWorker(resultChan chan Result, errChan chan error, urlChan chan string, downloadChan chan []byte, o ...Option) {
 	for {
-		// Считываем урл
-		url, err := readln(reader)
-		if err == io.EOF {
+		// Получаем url из main горутины
+		url, isOpen := <-urlChan
+		if !isOpen {
 			break
 		}
-		if err != nil {
-			err = fmt.Errorf("error: %v url: %v", err, url)
-			errChan <- err
-			return
+		// Инициируем рейтинг
+		rating := make(map[string]int)
+		result := Result{
+			Url: url,
+		}
+		for {
+			// Получаем частями тело ответа
+			bytes := <-downloadChan
+			if bytes == nil {
+				break
+			}
+
+			// Достаём из части текст
+			text, err := extractText(bytes)
+			if err != nil {
+				err = fmt.Errorf("error: %v url: %v", err, url)
+				errChan <- err
+				return
+			}
+
+			// Дробим текст на слова
+			words := spliter(text)
+
+			// Проходимся по всем словам и считаем их количество
+			for _, w := range words {
+				var up = upCount(w)
+				// Если заглавных букв больше 1 и это не абривиатура
+				if up > 1 && up != utf8.RuneCountInString(w) {
+
+					// ????? : Будут ли при этом слова проитерированы в этом цикле?
+
+					words = append(words, camelcase.Split(w)...)
+					continue
+				}
+				w = strings.ToLower(w)
+				// Проверяем строку на количество символов и является ли она словом
+				if utf8.RuneCountInString(w) > 3 && isWord(w) {
+					rating[w] += 1
+				} else {
+					continue
+				}
+
+				// Если количество упомянаний этого слова больше максимума
+				if rating[w] > result.Count[0] {
+					// 1, 2, 3 -> new, 1, 2
+					result.Count[1], result.Count[2] = result.Count[0], result.Count[1]
+					result.Words[1], result.Words[2] = result.Words[0], result.Words[1]
+
+					result.Count[0] = rating[w]
+					result.Words[0] = w
+					continue
+					// Если количество упомянаний этого слова больше 2-ого максимума
+				} else if rating[w] > result.Count[1] {
+					// 1, 2, 3 -> 1, new, 2
+					result.Count[2] = result.Count[1]
+					result.Words[2] = result.Words[1]
+
+					result.Count[1] = rating[w]
+					result.Words[1] = w
+					continue
+					// Если количество упомянаний этого слова больше 3-ого максимума
+				} else if rating[w] > result.Count[2] {
+					// 1, 2, 3 -> 1, 2, new
+					result.Count[2] = rating[w]
+					result.Words[2] = w
+					continue
+				}
+			}
+
 		}
 
-		result, err := ForPage(url, o...)
-
-		if err != nil {
-			err = fmt.Errorf("error: %v url: %v", err, url)
-			errChan <- err
-			return
-		}
-
-		// Отправляем результат main горутине, что всё хорошо
-		errChan <- nil
 		// Отправляем результат writeWorker горутине
 		resultChan <- result
 	}
@@ -54,25 +113,26 @@ func writeWorker(resultChan chan Result, errChan chan error, encoder *json.Encod
 			errChan <- err
 			return
 		}
-		// Отправляем результат main горутине, что всё хорошо
-		errChan <- nil
 	}
 }
 
 func downloadWorker(byteChan chan []byte, urlChan chan string, errChan chan error, client *http.Client) {
 
 	for {
+		// Получаем урл
 		url, isOpen := <-urlChan
 		if !isOpen {
 			break
 		}
 
+		// Формируем запрос
 		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
 			errChan <- err
 			return
 		}
 
+		// Отпарвляем запрос
 		res, err := client.Do(req)
 		if err != nil {
 			errChan <- err
@@ -80,18 +140,22 @@ func downloadWorker(byteChan chan []byte, urlChan chan string, errChan chan erro
 		}
 		defer res.Body.Close()
 
+		// Инициализируем ридер
 		reader := bufio.NewReader(res.Body)
 
 		for {
-			// Read the next chunk
+			// Считываем построчно тело запроса
 			bytes, err := reader.ReadBytes('\n')
 			if err == io.EOF {
+				byteChan <- nil
 				break
 			}
 			if err != nil {
 				errChan <- err
 				return
 			}
+
+			// Отправляем байты в processWorker
 			byteChan <- bytes
 		}
 	}
